482d39bca7ba3e598b44f11be891b1baf51d4d54
SohanJoshi <sohanjoshi1991@gmail.com>
2020-05-24 01:10:59 +0530

    Adding comment to the application

--- spring-boot-jwt-security/src/main/java/com/sohan/security/jwtsecurity/SpringBootJwtSecurityApplication.java
+++ spring-boot-jwt-security/src/main/java/com/sohan/security/jwtsecurity/SpringBootJwtSecurityApplication.java
@@ -3,6 +3,11 @@ package com.sohan.security.jwtsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The exception here is not working. Not sure why. The exception message is not being sent in response.
+ * The first request /token sends back the token, using the secret key and the JWTUser details int eh POST request.
+ * The same token when sent back in any request to /rest/** authorizes the request.
+ */
 @SpringBootApplication
 public class SpringBootJwtSecurityApplication {
 
